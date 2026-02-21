33ebc19c2a9506063df8986a96faa850cf308e83
Francisco <github.com.visa669@passinbox.com>
2026-02-21 19:29:44 +0100

    Add pages

--- app/utils/constants/routes.ts
+++ app/utils/constants/routes.ts
@@ -1,3 +1,12 @@
 export const ROUTES = {
   home: 'index',
+  books: 'books',
+  bookId: 'books-id',
+  dashboard: 'dashboard',
+  favorites: 'favorites',
+  pricing: 'pricing',
+  checkout: 'pricing-checkout',
+  signIn: 'sign-in',
+  signInAdmin: 'sign-in-admin',
+  signUp: 'sign-up',
 };
